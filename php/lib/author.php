7e2a8ed3aa149ea651cc0bab10a5a6ca6c6cd356
Trystan Gray <trystangray7@gmail.com>
2019-04-23 15:39:58 -0600

    working on boss fight

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,7 +1,4 @@
-<!--
-	namespace tgray19\ObjectOriented;
-
---><?php /*include("Author.php");
+<?php namespace tgray19\ObjectOriented; /*include("Author.php");
 	$stefan = new \tgray19\ObjectOriented\ Author(251561,"aviurl","none",
 		"testattest", "password", "Stefan M");
 	$jimmy = new \tgray19\ObjectOriented\Author(335695, "avatar", "none",
@@ -14,8 +11,12 @@
 	echo "Stefan's full name: " . $stefan->getAuthorUsername();
 	echo "Nick's full name: " . $jimmy->getAuthorUsername();
 */?>
-<?php var_dump(get_object_vars({$object}));
-$oVal = (object)[\tgray19\ObjectOriented\Author::class];
+<?php
+require_once("Author.php");
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+use Ramsey\Uuid\Uuid;
+use tgray19\ObjectOriented\Author;
 $object = new \tgray19\ObjectOriented\Author("9bfacccd-75fe-413e-9886-2bf4e0d7f89e",
 	"avatar", "none ", "newemail",
 	"dumb dumb", "newAuthorUsername");
