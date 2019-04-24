63c85a45f1f6699633fa28c62bad25ce06dfdcc1
Trystan Gray <trystangray7@gmail.com>
2019-04-23 21:43:57 -0600

    played around with boss fight with var dump

--- php/lib/author.php
+++ php/lib/author.php
@@ -1,22 +1,14 @@
-<?php namespace tgray19\ObjectOriented; /*include("Author.php");
-	$stefan = new \tgray19\ObjectOriented\ Author(251561,"aviurl","none",
-		"testattest", "password", "Stefan M");
-	$jimmy = new \tgray19\ObjectOriented\Author(335695, "avatar", "none",
-	"emailbro", "password1", "Nick W"
-	);
-
-	$stefan-> setAuthorUsername("Stefan Mischook");
-	$jimmy->setAuthorUsername("Nick Waddles");
-
-	echo "Stefan's full name: " . $stefan->getAuthorUsername();
-	echo "Nick's full name: " . $jimmy->getAuthorUsername();
-*/?>
 <?php
-require_once("Author.php");
+/**
+ * setup require_once , use statement, and call upon the author username, then
+ * var dump the new username out
+ *
+ */
+require_once(dirname(__DIR__) . "/vendor/autoload.php");
 require_once(dirname(__DIR__) . "/classes/autoload.php");
-
-use Ramsey\Uuid\Uuid;
 use tgray19\ObjectOriented\Author;
-$object = new \tgray19\ObjectOriented\Author("9bfacccd-75fe-413e-9886-2bf4e0d7f89e",
+$authorUsername = new Author("9bfacccd-75fe-413e-9886-2bf4e0d7f89e",
 	"avatar", "none ", "newemail",
-	"dumb dumb", "newAuthorUsername");
+	"T1B6Ymdqa3FJdmZqaDdqYghyC1jf2WjbgfD8Jp6GZE9Tg3IpsYpXKm2VWYOJq8LA", "newAuthorUsername");
+var_dump($authorUsername);
+
